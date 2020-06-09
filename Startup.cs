7a48005b74f4c0269ae95dcfe11d55e27f4901e6
Miyar NourAldeen <63904728+maiyarn@users.noreply.github.com>
2020-06-09 05:08:29 -0700

    Update Startup.cs
    try

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace commander
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see 
                 app.UseHsts();
             }
 
