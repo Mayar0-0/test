d52e47f902ee1c1387101685dd40020c12a2bdd6
Mayar <mearmh10@gmail.com>
2020-06-09 02:29:07 -0700

    init_commit

---
+++ Model/Command.cs
@@ -0,0 +1,13 @@
+namespace commander.model
+{
+
+    public class commander
+    {
+
+        [allowAnonymous]
+        public int id { get; set; }
+        public string command { get; set; }
+        public string howto { get; set; }
+
+    }
+}
